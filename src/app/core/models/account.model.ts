cc32908be90694de7d653131e9ae06712a3a2281
Vincent Nadoll <me@vinado.de>
2020-02-14 13:52:50 +0100

    Make credit property read-only

--- src/app/core/models/account.model.ts
+++ src/app/core/models/account.model.ts
@@ -2,7 +2,7 @@ import { Permission } from './permission.type';
 
 export interface Account {
   readonly id: string;
-  credit: number;
+  readonly credit: number;
   minimum_credit: number;
   name: string;
   mail: string;
