46488f1df13e706b288d3d4febc73dbfa12985b3
Lars Westermann <lars-westermann@live.de>
2020-08-14 15:42:38 +0200

    Implement customer info node

--- src/app/core/api/proxy.api.ts
+++ src/app/core/api/proxy.api.ts
@@ -2,16 +2,16 @@ import { requestJson, Method, BASE_URL } from "./utils";
 import { Product, Account } from "../models";
 
 export interface EventHandler {
-    onConnect(): any;
-    onDisconnect(): any;
+    onConnect?(): void;
+    onDisconnect?(): void;
 
-    onAccountScanned(account: Account): any;
-    onProductScanned(product: Product): any;
-    onBarCodeScanned(code: String): any;
-    onNfcCardAdded(id: String, writeable: boolean): any;
-    onNfcCardRemoved(): any;
-    onPaymentTokenGenerated(token: String): any;
-    onTimeout(): any;
+    onAccountScanned?(account: Account): void;
+    onProductScanned?(product: Product): void;
+    onBarCodeScanned?(code: String): void;
+    onNfcCardAdded?(id: String, writeable: boolean): void;
+    onNfcCardRemoved?(): void;
+    onPaymentTokenGenerated?(token: String): void;
+    onTimeout?(): void;
 }
 
 export async function requestPaymentToken(amount: number) {
@@ -34,6 +34,10 @@ export async function reauthenticate() {
 let eventHandlerList: EventHandler[] = [];
 
 export function registerEventHandler(handler: EventHandler) {
+    if (eventHandlerList.indexOf(handler) >= 0) {
+        return;
+    }
+
     if (eventHandlerList.length == 0) {
         eventHandlerList.push(handler)
 
@@ -43,31 +47,59 @@ export function registerEventHandler(handler: EventHandler) {
     }
 }
 
+export function removeEventHandler(handler: EventHandler) {
+    let index = eventHandlerList.indexOf(handler);
+    if (index >= 0) {
+        eventHandlerList.splice(index, 1);
+    }
+
+    if (eventHandlerList.length == 0) {
+        disconnectEventSource();
+    }
+}
+
+let evtSource: EventSource = null;
 function connectEventSource() {
-    const evtSource = new EventSource(BASE_URL + "/events");
+    if (evtSource) {
+        return;
+    }
+
+    evtSource = new EventSource(BASE_URL + "events");
     evtSource.onmessage = (event) => {
+        if (event.data === "connected" || event.data === "ping") {
+            return;
+        }
+        
         try {
             let data = JSON.parse(event.data);
             parseEventMessage(data);
         } catch (e) {
-            console.error("Cannot parse event message", e);
+            console.error("Cannot parse event message", e, event.data);
         }
     }
 
     evtSource.onopen = (_) => {
         for (let handler of eventHandlerList) {
-            handler.onConnect();
+            if (handler.onConnect) {
+                handler.onConnect();
+            }
         }
     }
 
     evtSource.onerror = (event) => {
         console.error("Connection to event source lost!", event);
         for (let handler of eventHandlerList) {
-            handler.onDisconnect();
+            if (handler.onDisconnect) {
+                handler.onDisconnect();
+            }
         }
         connectEventSource();
     }
+}
 
+function disconnectEventSource() {
+    evtSource.close();
+    evtSource = null;
 }
 
 function parseEventMessage(data: any) {
@@ -75,37 +107,51 @@ function parseEventMessage(data: any) {
         switch (data.type) {
             case "account":
                 for (let handler of eventHandlerList) {
-                    handler.onAccountScanned(data.content as Account);
+                    if (handler.onAccountScanned) {
+                        handler.onAccountScanned(data.content as Account);
+                    }
                 }
                 break;
             case "product":
                 for (let handler of eventHandlerList) {
-                    handler.onProductScanned(data.content as Product);
+                    if (handler.onProductScanned) {
+                        handler.onProductScanned(data.content as Product);
+                    }
                 }
                 break;
             case "qr-code":
                 for (let handler of eventHandlerList) {
-                    handler.onBarCodeScanned(data.content.code as String);
+                    if (handler.onBarCodeScanned) {
+                        handler.onBarCodeScanned(data.content.code as String);
+                    }
                 }
                 break;
             case "nfc-card":
                 for (let handler of eventHandlerList) {
-                    handler.onNfcCardAdded(data.content.id as String, data.content.writeable as boolean);
+                    if (handler.onNfcCardAdded) {
+                        handler.onNfcCardAdded(data.content.id as String, data.content.writeable as boolean);
+                    }
                 }
                 break;
             case "remove-nfc-card":
                 for (let handler of eventHandlerList) {
-                    handler.onNfcCardRemoved();
+                    if (handler.onNfcCardRemoved) {
+                        handler.onNfcCardRemoved();
+                    }
                 }
                 break;
             case "payment-token":
                 for (let handler of eventHandlerList) {
-                    handler.onPaymentTokenGenerated(data.content.token as String);
+                    if (handler.onPaymentTokenGenerated) {
+                        handler.onPaymentTokenGenerated(data.content.token as String);
+                    }
                 }
                 break;
             case "timeout":
                 for (let handler of eventHandlerList) {
-                    handler.onTimeout();
+                    if (handler.onTimeout) {
+                        handler.onTimeout();
+                    }
                 }
                 break;
         }
--- src/components/cashier/Customer.tsx
+++ src/components/cashier/Customer.tsx
@@ -1,13 +1,58 @@
 import * as React from "react";
 
 import "./Customer.scss";
+import { registerEventHandler, EventHandler, removeEventHandler } from "../../app/core/api";
+import { Account } from "../../app/core/models";
+import { MdCancel } from "react-icons/md";
 
 export interface CustomerProps { }
-export interface CustomerState { }
+export interface CustomerState {
+    account: Account
+}
 
-export class Customer extends React.Component<CustomerProps, CustomerState> {
+export class Customer extends React.Component<CustomerProps, CustomerState> implements EventHandler {
     static displayName = "Customer"
+
+    constructor(props: CustomerProps) {
+        super(props);
+
+        this.state = {
+            account: null
+        };
+    }
+
+    removeAccount() {
+        this.setState({
+            account: null
+        });
+    }
+
     render() {
-        return <div>Customer</div>;
+        if (this.state.account) {
+            return <div className="customer">
+                <div>
+                    <span>{this.state.account.name}</span>
+                    <span>{(this.state.account.credit / 100).toFixed(2)}€</span>
+                </div>
+                <div onClick={() => this.removeAccount()}><MdCancel /></div>
+            </div>;
+        } else {
+            return <div className="customer">No customer scanned!</div>;
+        }
+
+    }
+
+    onAccountScanned(account: Account) {
+        this.setState({
+            account
+        });
+    }
+
+    componentDidMount() {
+        registerEventHandler(this);
+    }
+
+    componentWillUnmount() {
+        removeEventHandler(this);
     }
 }
