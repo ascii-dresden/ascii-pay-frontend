4624ae6c6eaca736e27584830aa79b4d1bbb8d25
Lars Westermann <lars-westermann@live.de>
2020-09-20 13:00:00 +0200

    Add development auto reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-    mode: "production",
-
-    // Enable sourcemaps for debugging webpack's output.
+    mode: "development",
+    entry: {
+        app: './src/index.tsx',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].bundle.js',
+    },
     devtool: "source-map",
-
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({            
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+    ],
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js"]
     },
-
     module: {
         rules: [
             {
@@ -39,26 +54,13 @@ module.exports = {
             }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
     performance: {
         hints: false
     },
     devServer: {
-        // Enable compression
         compress: true,
-    
-        // Enable hot reloading
         hot: true,
-    
-        port: 3000
-      }
+        port: 3000,
+        contentBase: './dist',
+    },
 };
-
