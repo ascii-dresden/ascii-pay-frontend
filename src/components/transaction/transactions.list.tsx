2375f7592871f9b55086939c06892f70759eff34
Lars Westermann <git@lars-westermann.de>
2023-02-18 17:37:40 +0100

    Add balance chart

--- src/components/transaction/transactions.list.tsx
+++ src/components/transaction/transactions.list.tsx
@@ -12,21 +12,38 @@ import {
   Typography,
 } from "@mui/material";
 import { ShoppingCartOutlined } from "@mui/icons-material";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useGetAllTransactionsQuery } from "../../redux/api/accountApi";
 import FullScreenLoader from "../FullScreenLoader";
-import TransactionItem from "./transaction.component";
+import TransactionItem, {
+  addCoinAmount,
+  getTransactionSum,
+} from "./transaction.component";
 import { CreatePayment } from "./create-payment";
+import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import { CoinAmountView } from "../CoinAmountView";
+import { AccountDto, CoinAmountDto } from "../../redux/api/contracts";
+import { renderToString } from "react-dom/server";
 
-export const TransactionList = (props: { accountId: number }) => {
+type SeriesData = {
+  x: Date;
+  y: number;
+  price: CoinAmountDto;
+  beforeBalance: CoinAmountDto;
+  afterBalance: CoinAmountDto;
+  isStatic?: boolean;
+};
+
+export const TransactionList = (props: { account: AccountDto }) => {
   const [openModal, setOpenModal] = useState(false);
   const {
     isLoading,
     isError,
     error,
     data: transactions,
-  } = useGetAllTransactionsQuery(props.accountId);
+  } = useGetAllTransactionsQuery(props.account.id);
 
   useEffect(() => {
     if (isError) {
@@ -49,11 +66,139 @@ export const TransactionList = (props: { accountId: number }) => {
     return <FullScreenLoader />;
   }
 
+  const format = new Intl.DateTimeFormat("de-DE", {
+    dateStyle: "full",
+    timeStyle: "long",
+  });
+
   let sortedTransactions = [...transactions];
   sortedTransactions.reverse();
 
+  let seriesData: SeriesData[] = [];
+
+  let latest: SeriesData = {
+    x: new Date(),
+    y: props.account.balance.Cent ?? 0,
+    price: {},
+    beforeBalance: props.account.balance,
+    afterBalance: props.account.balance,
+    isStatic: true,
+  };
+  seriesData.push(latest);
+
+  let min = props.account.balance.Cent ?? 0;
+  let max = props.account.balance.Cent ?? 0;
+
+  for (let transaction of sortedTransactions) {
+    let price = getTransactionSum(transaction);
+    let afterBalance = latest.beforeBalance;
+    let beforeBalance = addCoinAmount(afterBalance, price);
+    latest = {
+      x: new Date(transaction.timestamp),
+      y: afterBalance.Cent ?? 0,
+      price,
+      beforeBalance,
+      afterBalance,
+    };
+    seriesData.push(latest);
+
+    if ((beforeBalance.Cent ?? 0) > max) {
+      max = beforeBalance.Cent ?? 0;
+    }
+    if ((beforeBalance.Cent ?? 0) < min) {
+      min = beforeBalance.Cent ?? 0;
+    }
+  }
+
+  seriesData.push({
+    x: new Date(latest.x.getTime() - 60 * 60 * 1000),
+    y: 0,
+    price: {},
+    beforeBalance: {},
+    afterBalance: {},
+    isStatic: true,
+  });
+
+  seriesData.reverse();
+
+  let series = [
+    {
+      data: seriesData,
+    },
+  ];
+  let options: ApexOptions = {
+    chart: {
+      type: "line",
+      height: 300,
+      toolbar: {
+        show: false,
+      },
+    },
+    stroke: {
+      curve: "stepline",
+    },
+    dataLabels: {
+      enabled: false,
+      formatter: function (val: number, opts) {
+        let price = seriesData[opts.dataPointIndex].price.Cent ?? 0;
+        return (price / 100).toFixed(2) + " €";
+      },
+    },
+    tooltip: {
+      shared: false,
+      custom: function ({ dataPointIndex }) {
+        let entry = seriesData[dataPointIndex];
+
+        if (entry.isStatic) {
+          return renderToString(
+            <div className="arrow_box">
+              <div>Balance</div>
+              <CoinAmountView coins={entry.afterBalance} />
+            </div>
+          );
+        }
+
+        let date = format.format(entry.y);
+        return renderToString(
+          <div className="arrow_box">
+            <div>{date}</div>
+            <div>Price</div>
+            <CoinAmountView coins={entry.price} />
+            <div>Before balance</div>
+            <CoinAmountView coins={entry.beforeBalance} />
+            <div>After balance</div>
+            <CoinAmountView coins={entry.afterBalance} />
+          </div>
+        );
+      },
+    },
+    markers: {
+      hover: {
+        sizeOffset: 4,
+      },
+    },
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: {
+      labels: {
+        formatter: function (val: number, opts) {
+          return (val / 100).toFixed(2) + " €";
+        },
+      },
+      min: Math.floor((min - 500) / 500) * 500,
+      max: Math.ceil((max + 500) / 500) * 500,
+    },
+  };
+
   return (
     <>
+      <Paper sx={{ p: 2, mb: 2 }}>
+        <CoinAmountView coins={props.account.balance} />
+        <div id="chart">
+          <Chart options={options} series={series} type="line" height={350} />
+        </div>
+      </Paper>
       <TableContainer component={Paper}>
         <Toolbar>
           <Typography sx={{ flex: "1 1 100%" }} variant="h6" component="div">
@@ -80,7 +225,7 @@ export const TransactionList = (props: { accountId: number }) => {
         </Table>
       </TableContainer>
       <CreatePayment
-        accountId={props.accountId}
+        accountId={props.account.id}
         open={openModal}
         setOpen={setOpenModal}
       />
