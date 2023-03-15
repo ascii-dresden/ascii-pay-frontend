a6c8ef1a2e2705ba0376116d9601909955f17039
Lars Westermann <git@lars-westermann.de>
2023-03-15 20:14:39 +0100

    Hide navigation drawer for none admin users

--- src/dashboardApp/pages/LoadingPage.tsx
+++ src/dashboardApp/pages/LoadingPage.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Box, Toolbar } from "@mui/material";
 import React from "react";
-import logo from "../../assets/ascii-pay-logo-wide.svg";
 import { FullScreenLoader } from "../components/FullScreenLoader";
 
 export const LoadingPage = () => {
@@ -10,15 +9,7 @@ export const LoadingPage = () => {
         position="fixed"
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
-        <Toolbar>
-          <Box sx={{ cursor: "pointer" }}>
-            <img
-              style={{ height: "2rem", marginTop: "0.5rem" }}
-              src={logo}
-              alt="ascii pay"
-            />
-          </Box>
-        </Toolbar>
+        <Toolbar></Toolbar>
       </AppBar>
       <FullScreenLoader />
     </Box>
