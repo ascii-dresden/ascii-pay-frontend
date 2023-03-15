90fe1a2a4ec3216d009f307b052088ab07b769a0
Lars Westermann <git@lars-westermann.de>
2023-03-15 13:39:21 +0100

    Show account in global transaction list

--- src/dashboardApp/components/transaction/TransactionListView.tsx
+++ src/dashboardApp/components/transaction/TransactionListView.tsx
@@ -242,9 +242,6 @@ const TransactionListRow = (props: { transaction: TransactionDto }) => {
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={3}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                Items
-              </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
